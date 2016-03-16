4f54d6a3201faf03befad4c093b391319e0b93cc
Nolan Darilek <nolan@thewordnerd.info>
2016-03-15 20:27:55 -0500

    Upgrade to Meteor 1.3-rc.1.

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -7,7 +7,6 @@ METEOR_WAREHOUSE_DIR="${METEOR_WAREHOUSE_DIR:-$HOME/.meteor}"
 METEOR_DEV_BUNDLE=$(dirname $(readlink -f "$METEOR_WAREHOUSE_DIR/meteor"))/dev_bundle
 
 cd /opt/app
-cp /home/vagrant/.meteor/packages/meteor-tool/1.1.13-beta.12/mt-os.linux.x86_64/dev_bundle/bin/node /opt/meteor-spk/meteor-spk.deps/bin
 "$METEOR_DEV_BUNDLE/bin/npm" install
 meteor build --directory /home/vagrant/
 (cd /home/vagrant/bundle/programs/server && "$METEOR_DEV_BUNDLE/bin/npm" install)
@@ -15,3 +14,4 @@ meteor build --directory /home/vagrant/
 # Copy our launcher script into the bundle so the grain can start up.
 mkdir -p /home/vagrant/bundle/opt/app/.sandstorm/
 cp /opt/app/.sandstorm/launcher.sh /home/vagrant/bundle/opt/app/.sandstorm/
+cp /home/vagrant/.meteor/packages/meteor-tool/1.2.4-rc.1/mt-os.linux.x86_64/dev_bundle/bin/node /opt/meteor-spk/meteor-spk.deps/bin
