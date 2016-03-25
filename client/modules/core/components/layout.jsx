90917a60ee7f67771b972151d42430967730b12f
Nolan Darilek <nolan@thewordnerd.info>
2016-03-25 14:48:41 -0500

    Push titles to Sandstorm.

--- client/modules/core/components/layout.jsx
+++ client/modules/core/components/layout.jsx
@@ -3,7 +3,13 @@ import {Col, Grid, Row} from "react-bootstrap"
 import Helmet from "react-helmet"
 
 export default (props) => <div>
-  <Helmet titleTemplate="%s - Alexandria"/>
+  <Helmet
+    titleTemplate="%s - Alexandria"
+    onChangeClientState={(state) => {
+      if(state.title)
+        window.parent.postMessage({"setTitle": state.title}, "*")
+    }}
+    />
   <Grid fluid={true}>
     <Row>
       <Col md={12}>
