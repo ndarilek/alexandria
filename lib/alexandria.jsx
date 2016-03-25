5bff0177a554974fb3b2e87d010ef209556cdbba
Nolan Darilek <nolan@thewordnerd.info>
2016-03-24 20:09:14 -0500

    Upgrade Meteor, and refactor to file-collection since CFS' future is uncertain.

--- lib/alexandria.jsx
+++ lib/alexandria.jsx
@@ -10,58 +10,13 @@ import toastr from "toastr"
 
 import {hasPermission} from "./sandstorm"
 
-if(Meteor.isServer) {
-  JsZip = require("jszip")
-  exec = require("child_process").exec
-  fs = require("fs")
-  parseString = require("xml2js").parseString
-  temp = require("temp")
-  temp.track()
-}
-
-const upload = new FS.Store.GridFS("upload")
-
-const htmlz = new FS.Store.GridFS("htmlz", {
-  beforeWrite: (fileObj) => ({
-    extension: "htmlz",
-    type: "application/zip"
-  }),
-  transformWrite: (fileObj, readStream, writeStream) => {
-    const tempDir = temp.mkdirSync()
-    const upload = fs.createWriteStream(tempDir+"/"+fileObj.name())
-    readStream.pipe(upload)
-    const baseName = upload.path
-    const htmlzName = baseName+".htmlz"
-    exec(`ebook-convert "${baseName}" "${htmlzName}"`, Meteor.bindEnvironment((err, stdout, stderr) => {
-      if(stdout)
-        console.log(`${stdout}`)
-      if(stderr)
-        console.error(`${stderr}`)
-      if(err)
-        console.error(`Error: ${err}`)
-      else {
-        const target = fs.createReadStream(htmlzName)
-        target.pipe(writeStream)
-        if(fs.existsSync(baseName))
-          fs.unlinkSync(baseName)
-        const zip = new JsZip(fs.readFileSync(htmlzName))
-        const metadata = zip.file("metadata.opf").asText()
-        parseString(metadata, {explicitArray: false, mergeAttrs: true, charkey: "char"}, (err, result) => {
-          if(err)
-            console.error(`Error: ${err}`)
-          else
-            fileObj.update({$set: {"metadata.original": result.package.metadata, formatVersion: 0}})
-          if(fs.existsSync(htmlzName))
-            fs.unlinkSync(htmlzName)
-        })
-      }
-    }))
-  }
-})
-
-Books = new FS.Collection("books", {stores: [upload, htmlz]})
+Books = new Mongo.Collection("books")
 
 export {Books}
+
+Uploads = new FileCollection("uploads", {resumable: true})
+
+export {Uploads}
 
 const title = (book) => {
   if(book.metadata)
@@ -70,9 +25,9 @@ const title = (book) => {
     else if(book.metadata.original && book.metadata.original["dc:title"])
       return book.metadata.original["dc:title"]
     else
-      return book.name()
+      return book.files.uploadFilename
   else
-    return book.name()
+    return book.files.uploadFilename
 }
 
 const author = (book) => {
@@ -121,7 +76,7 @@ export const BookListUI = ({books, canUpload, canRemove, remove}) => <div>
 const BookListContainer = (props, onData) => {
   if(Meteor.subscribe("books").ready())
     onData(null, {
-      books: Books.find({}, {sort: {"metadata.user.title": 1, "metadata.original.dc:title": 1, "original.name": 1}}).fetch(),
+      books: Books.find({}, {sort: {"metadata.user.title": 1, "metadata.original.dc:title": 1, "files.uploadFilename": 1}}).fetch(),
       canUpload: hasPermission("modify"),
       canRemove: hasPermission("modify"),
       remove: (id) => (() => Books.remove(id))
@@ -130,25 +85,24 @@ const BookListContainer = (props, onData) => {
 
 export const BookList = composeWithTracker(BookListContainer)(BookListUI)
 
-const UploadUI = React.createClass({
+class UploadUI extends React.Component {
 
-  form: forms.Form.extend({
-    file: forms.FileField({widgetAttrs: {autoFocus: true}})
-  }),
-
-  onSubmit(e) {
-    e.preventDefault()
-    const form = this.refs.form.getForm()
-    if(form.validate())
-      this.props.onSubmit(form.cleanedData)
-  },
+  componentDidMount() {
+    Uploads.resumable.assignBrowse(this.refs.file)
+    Uploads.resumable.on("fileAdded", this.props.fileAdded)
+    Uploads.resumable.on("progress", (progress) => this.setState({progress}))
+  }
 
   render() {
-    if(this.props.uploadProgress != null)
+    if(this.props.converting)
+      return <div>
+        <h1>Converting...</h1>
+      </div>
+    else if(this.state && this.state.progress != null)
       return <div>
         <Helmet title="Uploading"/>
         <h1>Uploading...</h1>
-        <progress value={this.props.uploadProgress/100}/>
+        <progress value={this.state.progress}/>
       </div>
     else
       return <div>
@@ -159,39 +113,34 @@ const UploadUI = React.createClass({
           </Navbar.Header>
         </Navbar>
         <h1>Upload Book</h1>
-        <form onSubmit={this.onSubmit}>
-          <forms.RenderForm ref="form" form={this.form}>
-            <BootstrapForm/>
-          </forms.RenderForm>
-          <Button type="submit">Upload</Button>
-        </form>
+        <input type="file" ref="file"/>
       </div>
   }
 
-})
-
-let updateUploadProgress = null
+}
 
 const UploadContainer = (props, onData) => {
-  onData(null, {
-    onSubmit: (args) => {
-      Books.insert(args.file, (err, fileObj) => {
-        if(err) {
-          console.log(err)
-          toastr.error(err)
-        } else
-          fileObj.on("uploaded", () => browserHistory.push(`/books/${fileObj._id}`))
-          onData(null, {uploadProgress: fileObj.uploadProgress()})
-          updateUploadProgress = setInterval(() => onData(null, {uploadProgress: fileObj.uploadProgress()}), 1000)
-      })
-    }
-  })
-  return () => {
-    if(updateUploadProgress) {
-      clearInterval(updateUploadProgress)
-      updateUploadProgress = null
-    }
+  data = {}
+  data.fileAdded = (file) => {
+    Uploads.insert({
+      _id: file.uniqueIdentifier,
+      filename: file.fileName,
+      contentType: file.file.type
+    }, (err) => {
+      if(err)
+        console.error(err)
+      else {
+        Uploads.resumable.on("complete", () => {
+          data.converting = true
+          Meteor.promise("books.create", file.uniqueIdentifier)
+          .then((id) => browserHistory.push(`/books/${id}`))
+          onData(null, data)
+        })
+        Uploads.resumable.upload()
+      }
+    })
   }
+  onData(null, data)
 }
 
 export const Upload = composeWithTracker(UploadContainer)(UploadUI)
--- server/alexandria.coffee
+++ server/alexandria.coffee
@@ -1,19 +1,36 @@
+Future = Npm.require("fibers/future")
 JSZip = require("jszip")
+exec = require("child_process").exec
+fs = require("fs")
 mime = require("mime")
+parseString = require("xml2js").parseString
+temp = require("temp")
 
-Books = require("/lib/alexandria.jsx").Books
+temp.track()
+
+Books = require("/lib/alexandria").Books
+Uploads = require("/lib/alexandria").Uploads
 
 # TODO: Fix me
-Books.allow
+Uploads.allow
   insert: ->
     @connection?.sandstormUser()?.permissions?.indexOf("modify") != -1
-  update: ->
-    @connection?.sandstormUser()?.permissions?.indexOf("modify") != -1
+  read: ->
+    true
+  write: ->
+    true
   remove: ->
     @connection?.sandstormUser()?.permissions?.indexOf("modify") != -1
 
+Htmlz = new FileCollection("htmlz")
+
+Books.find().observe
+  removed: (doc) ->
+    Uploads.remove(doc?.files?.uploadId?)
+    Htmlz.remove(doc?.files?.htmlzId?)
+
 Meteor.publish "books", ->
-  Books.find({}, {fields: {"original.name": 1, metadata: 1}})
+  Books.find({})
 
 Picker.route "/files/:id", (params, req, res) ->
   if @connection?.sandstormUser()?.permissions?.indexOf("download") == -1
@@ -21,11 +38,10 @@ Picker.route "/files/:id", (params, req, res) ->
     return res.end()
   book = Books.findOne(params.id)
   if book?
-    while !book.hasStored("upload")
-      continue
-    stream = book.createReadStream("upload")
-    res.setHeader("Content-Type", mime.lookup(book.original.name))
-    res.setHeader("Content-Disposition", "inline; filename="+encodeURI(book.original.name))
+    upload = Uploads.findOne(new Mongo.ObjectID(book.files?.uploadId?))
+    stream = Uploads.findOneStream(book.files?.uploadId?)
+    res.setHeader("Content-Type", mime.lookup(upload.filename))
+    res.setHeader("Content-Disposition", "inline; filename="+encodeURI(upload.filename))
     stream.pipe(res)
   else
     res.statusCode = 404
@@ -34,9 +50,7 @@ Picker.route "/files/:id", (params, req, res) ->
 Picker.route "/files/:id/:filename+", (params, req, res) ->
   book = Books.findOne(params.id)
   if book?
-    while !book.hasStored("htmlz")
-      continue
-    stream = book.createReadStream("htmlz")
+    stream = Htmlz.findOneStream(new Mongo.ObjectID(book.files?.htmlzId))
     buffer = new Buffer(0)
     filename = params.filename
     self = @
@@ -74,3 +88,46 @@ Meteor.methods
         throw new Meteor.Error(404, "Not found")
     else
       throw new Meteor.Error(403, "Unauthorized")
+
+Meteor.methods
+  "books.create": (id) ->
+    doc = Uploads.findOne(new Mongo.ObjectID(id))
+    tempDir = temp.mkdirSync()
+    baseName = tempDir+"/"+doc.filename
+    future = new Future()
+    Uploads.exportFile doc._id, baseName, (err, upload) ->
+      if err?
+        console.error(err)
+        future.throw(err)
+      else
+        htmlzName = baseName+".htmlz"
+        exec 'ebook-convert "'+baseName+'" "'+htmlzName+'"', Meteor.bindEnvironment((err, stdout, stderr) ->
+          if stdout?
+            console.log(stdout.toString())
+          if stderr? and stderr.toString().length
+            console.error(stderr.toString())
+          if err?
+            console.error("Error: ",err)
+            future.throw(err)
+          else
+            if fs.existsSync(baseName)
+              fs.unlinkSync(baseName)
+            bookId = Books.insert({files: {uploadId: id, uploadFilename: doc.filename}, formatVersion: 0})
+            Htmlz.importFile htmlzName, {contentType: mime.lookup(htmlzName)}, (err, file) ->
+              if err?
+                console.error(err)
+                future.throw(err)
+              else
+                Books.update(bookId, {$set: {"files.htmlzId": file._id.toHexString()}})
+                zip = new JSZip(fs.readFileSync(htmlzName))
+                metadata = zip.file("metadata.opf").asText()
+                parseString metadata, {explicitArray: false, mergeAttrs: true, charkey: "char"}, (err, result) ->
+                  if err?
+                    console.error(err)
+                  else
+                    Books.update(bookId, {$set: {"metadata.original": result.package.metadata}})
+                  if fs.existsSync(htmlzName)
+                    fs.unlinkSync(htmlzName)
+                  future.return(bookId)
+        )
+    future.wait()
